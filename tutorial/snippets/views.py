f134951f8a83738689b4f3e0683b6a4451c3143b
Julia Seidman <juliaseid@gmail.com>
2020-11-03 12:43:41 -0800

    apply permissions to views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 
@@ -28,4 +29,5 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly]
