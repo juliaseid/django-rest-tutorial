0d5d108b4399254af9c7b08c6c04b9db3a74cbd3
Julia Seidman <juliaseid@gmail.com>
2020-11-03 13:30:33 -0800

    add urls for highlights

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -8,10 +8,15 @@ urlpatterns = [
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
 ]
+
+
