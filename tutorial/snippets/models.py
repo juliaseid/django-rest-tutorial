49226176acc4a36ae06162ca2463c2ae1a2cd22f
Julia Seidman <juliaseid@gmail.com>
2020-11-03 10:59:21 -0800

    create superuser and add UserSerializer

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -24,14 +24,11 @@ class Snippet(models.Model):
         ordering = ['created']
 
     def save(self, *args, **kwargs):
-    """
-    Use the `pygments` library to create a highlighted HTML
-    representation of the code snippet.
-    """
-    lexer = get_lexer_by_name(self.language)
-    linenos = 'table' if self.linenos else False
-    options = {'title': self.title} if self.title else {}
-    formatter = HtmlFormatter(style=self.style, linenos=linenos,
-                              full=True, **options)
-    self.highlighted = highlight(self.code, lexer, formatter)
-    super(Snippet, self).save(*args, **kwargs)
+
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
